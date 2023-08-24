c2b3f854223c147aecae794b581e055d1c5fd048
monika <marihalm47@gmail.com>
2023-08-24 01:22:17 -0500

    clone commited the code

--- src/test/java/Sampletest/Githubsampletest.java
+++ src/test/java/Sampletest/Githubsampletest.java
@@ -5,6 +5,11 @@ public class Githubsampletest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Github employee X.");
+
+System.out.println("Github employee test 1.");
+System.out.println("Github employee test 2.");
+System.out.println("Github employee test 2.");
+
 	}
 
 }
